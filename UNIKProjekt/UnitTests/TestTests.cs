e1d798b57c0345815b7a092b3007967c8a76ed43
Lloyd <lloydbh@outlook.dk>
2020-12-17 18:15:44 +0100

    Tilrettet Testdata
    Har fjernet testen til EmptyName. Har tilføjet test til DoesAlreadyExistInDatabase. Der er nu test for at en bruger allerede eksistere, og for at den ikke allerede eksistere.

--- UNIKProjekt/UnitTests/TestTests.cs
+++ UNIKProjekt/UnitTests/TestTests.cs
@@ -1,42 +1,12 @@
 ﻿using Application.Handlers;
 using Domain.Models;
-using Infrastructure.Repositories;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using Xunit;
 
 namespace UnitTests
 {
     public class TestTests
     {
-        
-
-        //should receive an exception, because the name is an empty string.
-        [Fact]
-        //[ExpectedException(typeof(ArgumentException))]
-        public void Test_CreateNewUser_EmptyName()
-        {
-            User user = User.CreateNewUser("", "Testerson", "Tester@testerson.com", "123pw", "saltsaltsaltsalt");
-
-            var mock = new Mock<IUserHandler>();
-            mock.SetupAdd(x => x.CreateUser(user));
-
-            Assert.Throws<ArgumentException>(() => user.Fname);
-        }
-
-        //Should also receive an exception, because the user already exists.
-        //[Fact]
-        ////[ExpectedException(typeof(ArgumentException))]
-        //public void Test_CreateNewUser_AlreadyExistsInDatabase()
-        //{
-        //    User user = User.CreateNewUser("Tester", "Testerson", "Tester@testerson.com", "123pw", "saltsaltsaltsalt");
-        //    var mock = new Mock<IUserHandler>();
-        //    mock.Setup(x => x.DoesAlreadyExistInDatabase()).Returns(true);
-
-        //    Assert.Throws<ArgumentException>
-
-        //}
 
         [Fact]
         public void Test_CreateNewUser_DoesNotAlreadyExistInDatabase()
@@ -52,24 +22,20 @@ namespace UnitTests
             Assert.Equal("123pw", user.Password);
             Assert.Equal("saltsaltsaltsalt", user.Salt);
         }
-        
-        
-        //[TestMethod]
-        //[ExpectedException(typeof(ArgumentException))]
 
+        [Fact]
+        public void Test_CreateNewUser_DoesAlreadyExistInDatabase()
+        {
+            User user = User.CreateNewUser("Tester", "Testerson", "Tester@testerson.com", "123pw", "saltsaltsaltsalt");
 
-        //[Fact]
-        //public void PassingTest()
-        //{
-        //}
-        //[Fact]
-        //public void FailingTest()
-        //{
-        //    Assert.Equal(5, Add(3, 2));
-        //}
-        //int Add(int x, int y)
-        //{
-        //    return x + y;
-        //}
+            var mock = new Mock<IUserHandler>();
+            mock.Setup(x => x.DoesAlreadyExistInDatabase()).Returns(true);
+
+            Assert.Equal("Tester", user.Fname);
+            Assert.Equal("Testerson", user.Lname);
+            Assert.Equal("Tester@testerson.com", user.Email);
+            Assert.Equal("123pw", user.Password);
+            Assert.Equal("saltsaltsaltsalt", user.Salt);
+        }
     }
 }
