d7843f189b6dd5c40282e8362a66c43a73c84834
Jesper Wolder <jesperwolder@yahoo.dk>
2020-12-22 20:05:58 +0100

    Lasse fix waitlist :)

--- UNIKProjekt/Application/Handlers/WaitingListHandler.cs
+++ UNIKProjekt/Application/Handlers/WaitingListHandler.cs
@@ -29,7 +29,7 @@ namespace Application.Handlers
 
         public void Create(WaitingList list)
         {
-            var User = Context.Users.Where(x => x.UserID == list.UserID).FirstOrDefault();
+            var User = Context.UserDetails.Where(x => x.UserID == list.UserID).FirstOrDefault();
             if (User == null)
                 throw new Exception("Brugeren blev ikke fundet");
 
@@ -37,12 +37,16 @@ namespace Application.Handlers
             if (Apartment == null)
                 throw new Exception("Apartment not found");
 
+            var Goals = Context.ApplicantGoals.Where(x => x.GoalsID == Apartment.ApplicantGoalsID).FirstOrDefault();
+            if (Goals == null)
+                throw new Exception("Du kan ikke skrive dig op til dette lejemål.");
+
             WaitingList newWL = new WaitingList()
             {
                 WaitingID = Guid.NewGuid().ToString(),
                 UserID = list.UserID,
                 ApartmentID = list.ApartmentID,
-                //ApplicationScore = ScoreApplication(Apartment.ApplicantGoals, User)
+                ApplicationScore = ScoreApplication(Goals, User)
             };
 
             Context.WaitingList.Add(newWL);
@@ -73,14 +77,24 @@ namespace Application.Handlers
             return AllAparmentsWritenUp;
         }
 
-        public double ScoreApplication(ApplicantGoals goals, User user)
+        public double ScoreApplication(ApplicantGoals goals, UserDetails user)
         {
-            Age AgeOfApplicant = new Age(user.UserDetails.Birthdate);
-            Age TargetAge = new Age(goals.Birthdate);
+            int TargetAge = DateTime.Now.Year - goals.Birthdate.Year;
+            if (DateTime.Now.DayOfYear < goals.Birthdate.DayOfYear)
+            {
+                TargetAge = TargetAge - 1;
+            }
+
+            int AgeOfApplicant = DateTime.Now.Year - user.Birthdate.Year;
+            if (DateTime.Now.DayOfYear < user.Birthdate.DayOfYear)
+            {
+                AgeOfApplicant = AgeOfApplicant - 1;
+            }
+
             //Remember to add factors here
-            int petMatch = user.UserDetails.Animals ? Convert.ToInt32(goals.Animals) : 1;
-            double ageMatch = (Math.Abs(AgeOfApplicant.Value - TargetAge.Value)) / TargetAge.Value;
-            double hasComment = user.UserDetails.Comment != null ? 1.3 : 1;
+            int petMatch = user.Animals ? Convert.ToInt32(goals.Animals) : 1;
+            double ageMatch = (Math.Abs(AgeOfApplicant - TargetAge)) / TargetAge;
+            double hasComment = user.Comment != null ? 1.3 : 1;
 
             return petMatch * ageMatch * hasComment;
         }
